5a9555bb2f1c6a50e5969570a4076ea46c87f04c
Maëlick Claes <himself@maelick.net>
2024-08-01 17:42:59 +0000

    buffered reader handles filenames ending in .gz as gziped files

--- load/buffered_reader.go
+++ load/buffered_reader.go
@@ -2,7 +2,10 @@ package load
 
 import (
 	"bufio"
+	"compress/gzip"
+	"io"
 	"os"
+	"strings"
 )
 
 const (
@@ -12,15 +15,28 @@ const (
 // GetBufferedReader returns the buffered Reader that should be used by the file loader
 // if no file name is specified a buffer for STDIN is returned
 func GetBufferedReader(fileName string) *bufio.Reader {
-	if len(fileName) == 0 {
-		// Read from STDIN
-		return bufio.NewReaderSize(os.Stdin, defaultReadSize)
-	}
-	// Read from specified file
-	file, err := os.Open(fileName)
+	file, err := fileReader(fileName)
 	if err != nil {
 		fatal("cannot open file for read %s: %v", fileName, err)
 		return nil
 	}
 	return bufio.NewReaderSize(file, defaultReadSize)
+}
+
+// fileReader returns a reader for the given file name,
+// handling gzipped files if the file extensions is gzip,
+// or STDIN if the file name is empty
+func fileReader(fileName string) (io.Reader, error) {
+	if len(fileName) == 0 {
+		// Read from STDIN
+		return os.Stdin, nil
+	}
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	if !strings.HasSuffix(fileName, ".gz") {
+		return file, nil
+	}
+	return gzip.NewReader(file)
 }

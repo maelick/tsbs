6beb79a00b5bea6ea8caccac774f140083cd51e2
Maëlick Claes <himself@maelick.net>
2024-08-02 12:04:43 +0000

    decrease iot dataset scale to 1000

--- benchmark/run_timescaledb.sh
+++ benchmark/run_timescaledb.sh
@@ -9,6 +9,7 @@ gen_data=${bin_dir}/tsbs_generate_data
 gen_queries=${bin_dir}/tsbs_generate_queries
 tsbs_load=${bin_dir}/tsbs_load
 load_script=${scripts_dir}/load/load_timescaledb.sh
+tsbs_run=${bin_dir}/tsbs_run_queries_timescaledb
 
 # start and wait for timescaledb to be ready
 docker compose up -d timescaledb
@@ -23,15 +24,15 @@ cd timescaledb
 # generate data and queries if missing
 
 if [ ! -f "timescaledb/iot-data.gz" ]; then
-    time $gen_data --use-case="iot" --seed=123 --scale=4000 \
+    time $gen_data --use-case="iot" --seed=123 --scale=1000 \
         --timestamp-start="2016-01-01T00:00:00Z" \
         --timestamp-end="2016-01-04T00:00:00Z" \
         --log-interval="10s" --format="timescaledb" \
         | gzip > iot-data.gz
 fi
 
-if [ ! -f "timescaledb/iot-queries.gz" ]; then
-    time $gen_queries --use-case="iot" --seed=123 --scale=4000 \
+if [ ! -f "timescaledb/iot-queries-breakdown-frequency.gz" ]; then
+    time $gen_queries --use-case="iot" --seed=123 --scale=1000 \
         --timestamp-start="2016-01-01T00:00:00Z" \
         --timestamp-end="2016-01-04T00:00:01Z" \
         --queries=1000 --query-type="breakdown-frequency" --format="timescaledb" \
@@ -50,8 +51,12 @@ psql() {
 # drop existing database and load data
 psql -d postgres -c "DROP DATABASE IF EXISTS benchmark;"
 
-time $tsbs_load load timescaledb --config=./iot-config.yaml | tee load.log
+time $tsbs_load load timescaledb --config=./iot-config.yaml | tee iot-load.log
+
+time (cat iot-queries-breakdown-frequency.gz | \
+    gunzip | $tsbs_run --workers=1 \
+        --postgres="host=localhost user=postgres password=postgres sslmode=disable") | tee iot-queries-breakdown-frequency.log
 
 # cleanup
 docker compose down
-docker volume rm benchmark_timescaledb-volume
+docker volume rm benchmark_timescaledb-data

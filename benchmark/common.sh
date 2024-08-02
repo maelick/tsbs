2a0758cb9f527e59180e93074b30ae3b4bedb75b
Maëlick Claes <himself@maelick.net>
2024-08-02 12:45:37 +0000

    extract functions for generating, loading and running data/queries in common.sh

---
+++ benchmark/common.sh
@@ -0,0 +1,70 @@
+if [ "$0" = "/bin/bash" ]; then
+    benchmark_dir=$(pwd)
+else
+    benchmark_dir=$(dirname $0)
+fi
+bin_dir=${benchmark_dir}/../bin
+scripts_dir=${benchmark_dir}/../scripts
+gen_data=${bin_dir}/tsbs_generate_data
+gen_queries=${bin_dir}/tsbs_generate_queries
+tsbs_load=${bin_dir}/tsbs_load
+load_script=${scripts_dir}/load/load_timescaledb.sh
+
+gen_data() {
+    use_case=$1
+    format=$2
+    if [ -z "$use_case" ]; then
+        echo "Usage: gen_data <use_case> <format>"
+        return 1
+    fi
+    filename="$use_case-data.gz"
+    if [ ! -f $filename ]; then
+        time $gen_data --use-case=$use_case --seed=123 --scale=1000 \
+            --timestamp-start="2016-01-01T00:00:00Z" \
+            --timestamp-end="2016-01-04T00:00:00Z" \
+            --log-interval="10s" --format=$format \
+            | gzip > $filename
+    fi
+}
+
+gen_queries() {
+    use_case=$1
+    query_type=$2
+    format=$3
+    if [ -z "$use_case" ] || [ -z "$query_type" ] || [ -z "$format" ]; then
+        echo "Usage: gen_queries <use_case> <query_type> <format>"
+        return 1
+    fi
+    filename="$use_case-queries-$query_type.gz"
+    if [ ! -f $filename ]; then
+        time $gen_queries --use-case=$use_case --seed=123 --scale=1000 \
+            --timestamp-start="2016-01-01T00:00:00Z" \
+            --timestamp-end="2016-01-04T00:00:01Z" \
+            --queries=1000 --query-type=$query_type --format=$format \
+            | gzip > $filename
+    fi
+}
+
+load_data() {
+    use_case=$1
+    db=$2
+    if [ -z "$use_case" ] || [ -z "$db" ]; then
+        echo "Usage: load_data <use_case> <db>"
+        return 1
+    fi
+    time $tsbs_load load $db --config=./$use_case-config.yaml | tee $use_case-load.log
+}
+
+run_queries() {
+    use_case=$1
+    query_type=$2
+    db=$3
+    shift 3
+    if [ -z "$use_case" ] || [ -z "$query_type" ] || [ -z "$db" ]; then
+        echo "Usage: run_queries <use_case> <query_type> <db> [args]"
+        return 1
+    fi
+    filename="$use_case-queries-$query_type"
+    tsbs_run="${bin_dir}/tsbs_run_queries_$db"
+    time (cat "$filename.gz" | gunzip | $tsbs_run --workers=1 "$@") | tee "$filename.log"
+}
--- benchmark/run_timescaledb.sh
+++ benchmark/run_timescaledb.sh
@@ -3,13 +3,7 @@ if [ "$0" = "/bin/bash" ]; then
 else
     benchmark_dir=$(dirname $0)
 fi
-bin_dir=${benchmark_dir}/../bin
-scripts_dir=${benchmark_dir}/../scripts
-gen_data=${bin_dir}/tsbs_generate_data
-gen_queries=${bin_dir}/tsbs_generate_queries
-tsbs_load=${bin_dir}/tsbs_load
-load_script=${scripts_dir}/load/load_timescaledb.sh
-tsbs_run=${bin_dir}/tsbs_run_queries_timescaledb
+source ${benchmark_dir}/common.sh
 
 # start and wait for timescaledb to be ready
 docker compose up -d timescaledb
@@ -21,23 +15,10 @@ done
 
 cd timescaledb
 
-# generate data and queries if missing
+# generate data and queries
 
-if [ ! -f "timescaledb/iot-data.gz" ]; then
-    time $gen_data --use-case="iot" --seed=123 --scale=1000 \
-        --timestamp-start="2016-01-01T00:00:00Z" \
-        --timestamp-end="2016-01-04T00:00:00Z" \
-        --log-interval="10s" --format="timescaledb" \
-        | gzip > iot-data.gz
-fi
-
-if [ ! -f "timescaledb/iot-queries-breakdown-frequency.gz" ]; then
-    time $gen_queries --use-case="iot" --seed=123 --scale=1000 \
-        --timestamp-start="2016-01-01T00:00:00Z" \
-        --timestamp-end="2016-01-04T00:00:01Z" \
-        --queries=1000 --query-type="breakdown-frequency" --format="timescaledb" \
-        | gzip > iot-queries-breakdown-frequency.gz
-fi
+gen_data iot timescaledb
+gen_queries iot breakdown-frequency timescaledb
 
 psql() {
     if [ -z "$1" ] || [ "$1" = "-t" ]; then
@@ -51,11 +32,8 @@ psql() {
 # drop existing database and load data
 psql -d postgres -c "DROP DATABASE IF EXISTS benchmark;"
 
-time $tsbs_load load timescaledb --config=./iot-config.yaml | tee iot-load.log
-
-time (cat iot-queries-breakdown-frequency.gz | \
-    gunzip | $tsbs_run --workers=1 \
-        --postgres="host=localhost user=postgres password=postgres sslmode=disable") | tee iot-queries-breakdown-frequency.log
+load_data iot timescaledb
+run_queries iot breakdown-frequency --postgres="host=localhost user=postgres password=postgres sslmode=disable"
 
 # cleanup
 docker compose down
